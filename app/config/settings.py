fb705f8f6ab80db80317669f56ea93510fe44cd9
1Suphaset <suphaset.moth@northbkk.ac.th>
2025-11-06 21:19:28 +0700

    edit3

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,10 +2,10 @@ import os
 
 # BASE_DIR ชี้ไปที่ root ของโปรเจกต์ (โฟลเดอร์ FND)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # -> app/config/settings.py
-BASE_DIR = os.path.abspath(os.path.join(BASE_DIR, "../../"))  # ขึ้นไปสองระดับจาก app/config/
+BASE_DIR = os.path.abspath(os.path.join(BASE_DIR, "../"))  # ขึ้นไปสองระดับจาก app/config/
 
-MODEL_PATH = os.path.join(BASE_DIR, "Core", "Models", "result", "models", "model_fold_5.h5")
-VECTORIZER_PATH = os.path.join(BASE_DIR, "Core", "Features", "result", "vectorizer.pkl")
+MODEL_PATH = os.path.join(BASE_DIR,"assets", "model_fold_5_new.h5")
+VECTORIZER_PATH = os.path.join(BASE_DIR, "assets", "vectorizer-new.pkl")
 
 print("MODEL_PATH:", MODEL_PATH)
 print("VECTORIZER_PATH:", VECTORIZER_PATH)
